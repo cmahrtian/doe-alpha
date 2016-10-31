8a40f7616cd52d751de0326ed72e5253d3d95144
Chris Mahr <mahrtian@gmail.com>
2016-10-31 16:58:26 -0400

    Add files via GUI to repo

---
+++ script.js
@@ -0,0 +1,7 @@
+// $('.carousel.carousel-slider').carousel({full_width: true});
+
+$(document).ready(function(){
+  $('.slider').slider({height: 200});
+  $('.tooltipped').tooltip({delay: 50});
+  $(".dropdown-button").dropdown();
+});
