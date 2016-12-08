d8f0eb84d8969c7f8b119f8114a261e68e2fa990
Chris Mahr <CMahr@schools.nyc.gov>
2016-12-08 17:01:50 -0500

    render 2016 MOTP Value and Rating of selected teacher as indicated in TeacherController

--- public/scripts/controllers/TeacherCtrl.js
+++ public/scripts/controllers/TeacherCtrl.js
@@ -3,4 +3,12 @@ angular.module('TeacherCtrl', [])
 				setTimeout(function() {
 					jQuery('.dropdown-button').dropdown();
 				}, 0);
+
+				d3.csv('../data/Teachers.csv', function(data) {
+					// select our teacher
+					var teacher = data[0];
+					// edit the previous year's MOTP score and rating with teacher data
+					d3.select('.previous-motp .score').text(teacher['Y16_MOTP_Value']);
+					d3.select('.previous-motp .rating').text(teacher['Y16_MOTP_Rating']);
+				});
 			});
