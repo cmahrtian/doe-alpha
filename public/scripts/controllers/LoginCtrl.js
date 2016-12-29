da1b88fc07741931b02062754243c765b028830d
Chris Mahr <CMahr@schools.nyc.gov>
2016-12-29 12:51:00 -0500

    add very basic login functionality

--- public/scripts/controllers/LoginCtrl.js
+++ public/scripts/controllers/LoginCtrl.js
@@ -1,5 +1,20 @@
 angular.module('LoginCtrl', [])
-			.controller('LoginController', function($scope) {
-				// placeholder functionality
-        $scope.tagline = 'Nothing beats a pocket protector!';
+	.controller('LoginController', function($scope) {
+		jQuery('a.logon').click(function() {
+			var email = jQuery('#email').val();
+			var password = jQuery('#password').val();
+			d3.csv('../data/Teachers.csv', function(data) {
+				var emailAddresses = data.map(function(teacher) {
+					return teacher.Email;
+				});
+				var employeeIDs = data.map(function(teacher) {
+					return teacher.EmployeeID;
+				});
+				if (emailAddresses.includes(email) && employeeIDs.includes(password)) {
+					window.location.replace('/home');
+				} else {
+					alert('Invalid email address and/or user ID');
+				};
 			});
+		});
+	});
