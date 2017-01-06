62cca643c8e8bc21668861dca86bd27f963e4840
Chris Mahr <CMahr@schools.nyc.gov>
2017-01-06 12:49:33 -0500

    save logged-in teacher's employee ID as localStorage variable

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,19 +1,37 @@
 var app = angular.module('growthExplorer', ['ngRoute', 'ngStorage', 'appRoutes', 'HomeCtrl', 'LoginCtrl', 'TeacherCtrl', 'LoginService']);
 
-app.service('Login', function() {
-	// TBD login code
-	var credentials = {
-		email: '',
-		employeeID: ''
-	};
+// app.factory('Login', function($localStorage) {
+// 	$localStorage = $localStorage.$default({
+// 		credentials: {
+// 			email: '',
+// 			employeeID: ''
+// 		}
+// 	});
+// 	return {
+// 		getCredentials: function() {
+// 			return $localStorage.credentials;
+// 		},
+// 		setCredentials: function(email, employeeID) {
+// 			$localStorage.credentials.email = email;
+// 			$localStorage.credentials.employeeID = employeeID;
+// 		}
+// 	};
+// });
+
+// app.service('Login', function() {
+// TBD login code
+// 	var credentials = {
+// 		email: '',
+// 		employeeID: ''
+// 	};
 	
-	return {
-		getCredentials: function() {
-			return credentials;
-		},
-		setCredentials: function(email, employeeID) {
-			credentials.email = email;
-			credentials.employeeID = employeeID;
-		}
-	};
-});
+// 	return {
+// 		getCredentials: function() {
+// 			return credentials;
+// 		},
+// 		setCredentials: function(email, employeeID) {
+// 			credentials.email = email;
+// 			credentials.employeeID = employeeID;
+// 		}
+// 	};
+// });
