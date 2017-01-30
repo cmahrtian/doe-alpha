a1fe545a8561a9ff138d29dcf5c4ec749d94aacb
Chris Mahr <CMahr@schools.nyc.gov>
2017-01-30 12:28:20 -0500

    set up routing from Dig In button to components page

--- public/scripts/appRoutes.js
+++ public/scripts/appRoutes.js
@@ -1,23 +1,29 @@
 angular.module('appRoutes', [])
-			.config(['$routeProvider', '$locationProvider', 
-				function($routeProvider, $locationProvider) {
-					$routeProvider
-					// login page that will use the LoginController
-						.when('/', {
-							templateUrl: 'views/pages/login.ejs',
-							controller: 'LoginController'
-						})
-						.when('/home', {
-					// home page
-							templateUrl: 'views/pages/home.ejs',
-							controller: 'HomeController'
-						})
-						.when('/teacher-practice', {
-							templateUrl: 'views/pages/teacher-practice.ejs',
-							controller: 'TeacherController'
-						});
-					$locationProvider.html5Mode({
-						enabled: true,
-						requireBase: false
-					});		
-				}]);
+	.config(['$routeProvider', '$locationProvider', 
+		function($routeProvider, $locationProvider) {
+			$routeProvider
+				// login page that will use the LoginController
+				.when('/', {
+					templateUrl: 'views/pages/login.ejs',
+					controller: 'LoginController'
+				})
+				.when('/home', {
+				// home page
+					templateUrl: 'views/pages/home.ejs',
+					controller: 'HomeController'
+				})
+				// teacher practice page
+				.when('/teacher-practice', {
+					templateUrl: 'views/pages/teacher-practice.ejs',
+					controller: 'TeacherController'
+				})
+				// components page
+				.when('/teacher-practice/components', {
+					templateUrl: 'views/pages/components.ejs',
+					controller: 'TeacherController'
+				});
+			$locationProvider.html5Mode({
+				enabled: true,
+				requireBase: false
+			});		
+		}]);
